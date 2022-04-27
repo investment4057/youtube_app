acd47035459eb85dee840b8b1a4fec322eec26f0
investment4057 <investment4057@gmail.com>
2022-04-28 02:08:13 +0900

    チャンネル部分のレイアウト作成

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,48 @@ class MyApp extends StatelessWidget {
             ),
           ],
         ),
-        body: Container(),
+        body: Container(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(16),
+                child: Row(
+                  children: [
+                    Image.network(
+                      'https://uploads-ssl.webflow.com/6003a357378a533a9a2dada7/6056b0fcea0ff7f50c8f4b60_flutter_logo.png',
+                      width: 50,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Youtubeチャンネル',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            TextButton(
+                              onPressed: () {},
+                              child: Row(
+                                children: [
+                                  Icon(Icons.video_call),
+                                  Text('登録する'),
+                                ],
+                              ),
+                            ),
+                            Text('チャンネル登録者数2,500人')
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
