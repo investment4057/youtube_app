c8f2938c87780b297f653affe8138c2416e541af
investment4057 <investment4057@gmail.com>
2022-04-28 01:28:21 +0900

    テーマカラー変更

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Youtubeアプリ'),
